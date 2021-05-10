b25e0fb057c18b687a8e1b0956626903626bd7c0
slagrabos <sla.grabos@gmail.com>
2021-05-10 23:13:16 +0200

    initial commit

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            // throw new InvalidProgramException("Bad things happens to good developers");
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
